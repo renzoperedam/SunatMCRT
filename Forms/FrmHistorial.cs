e801b8ee543bf168bf338a0528dfbfb8973216ca
Dani <danielpalominosovero@gmail.com>
2026-07-07 16:01:18 -0500

    Mejorar presentacion del historial de comprobantes

--- Forms/FrmHistorial.cs
+++ Forms/FrmHistorial.cs
@@ -10,7 +10,35 @@ public partial class FrmHistorial : Form
     {
         _servicio = servicio;
         InitializeComponent();
+        ConfigurarHistorialGrid();
         CargarHistorial();
+    }
+
+    private void ConfigurarHistorialGrid()
+    {
+        dgvHistorial.ReadOnly = true;
+        dgvHistorial.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        dgvHistorial.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        dgvHistorial.MultiSelect = false;
+        dgvHistorial.AllowUserToAddRows = false;
+        dgvHistorial.AllowUserToDeleteRows = false;
+        dgvHistorial.AllowUserToResizeRows = false;
+        dgvHistorial.RowHeadersVisible = false;
+        dgvHistorial.AutoGenerateColumns = true;
+
+        dgvHistorial.AlternatingRowsDefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+        dgvHistorial.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(51, 153, 255);
+        dgvHistorial.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.White;
+        dgvHistorial.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+
+        dgvHistorial.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(0, 120, 215);
+        dgvHistorial.ColumnHeadersDefaultCellStyle.ForeColor = System.Drawing.Color.White;
+        dgvHistorial.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+        dgvHistorial.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+        dgvHistorial.EnableHeadersVisualStyles = false;
+        dgvHistorial.ColumnHeadersHeight = 36;
+        dgvHistorial.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
     }
 
     private void btnActualizar_Click(object sender, EventArgs e)
@@ -41,6 +69,5 @@ public partial class FrmHistorial : Form
 
     private void dgvHistorial_CellContentClick(object sender, DataGridViewCellEventArgs e)
     {
-
     }
 }
